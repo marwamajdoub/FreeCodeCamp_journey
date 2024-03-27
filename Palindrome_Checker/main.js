586910fc76f8e57e1759df8fbfdb601ab7254762
marwamajdoub <majdoub.maroua@gmail.com>
2024-03-27 16:13:53 +0100

    palindrome

---
+++ Palindrome_Checker/main.js
@@ -0,0 +1,21 @@
+
+function checkPalindrome() {
+    let str = document.getElementById('inputString').value;
+    let allsy = /[ ,.*#_$@!%^&()+=/;:?<>-]/g;
+    str = str.toLowerCase().replace(allsy, '');
+
+    let all = str.split('');
+    let reversed = [];
+
+    for (let i = all.length - 1; i >= 0; i--) {
+        reversed.push(all[i]);
+    }
+
+    let result = document.getElementById('result');
+    if (reversed.join('') === str) {
+        result.innerText = 'Yes, it\'s a palindrome!😊';
+    } else {
+        result.innerText = 'No, it\'s not a palindrome.😔';
+    }
+    
+}
