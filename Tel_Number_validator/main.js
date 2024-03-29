d6e84649e73e725f411cefd1ab8551dc0110fc69
marwamajdoub <majdoub.maroua@gmail.com>
2024-03-29 03:57:37 +0100

    first commit

---
+++ Tel_Number_validator/main.js
@@ -0,0 +1,11 @@
+function validatePhoneNumber() {
+    const phoneNumberInput = document.getElementById('phoneNumber').value.trim();
+    const resultDisplay = document.getElementById('result');
+    const regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s-]?\d{3}[\s-]?\d{4}$/;
+
+    if (regex.test(phoneNumberInput)) {
+        resultDisplay.innerText = "Valid US phone number!";
+    } else {
+        resultDisplay.innerText = "Invalid US phone number. Please enter a valid format.";
+    }
+}
